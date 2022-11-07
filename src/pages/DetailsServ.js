47b2f19781f5873075aadc86858b9295e69b83d8
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-11-08 01:03:47 +0600

    details service

--- src/pages/DetailsServ.js
+++ src/pages/DetailsServ.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router";
 
 const DetailsServ = () => {
-    const { id } = useParams();
-    return (
-        <div>
-            {id}
-        </div>
-    );
+  const { id } = useParams();
+  const [service, setService] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/services/${id}`)
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, [id]);
+  return (
+    <div className="flex gap-5 my-5 w-full justify-between">
+      <div className="w-1/2">
+        <img className="w-full" src={service.image} alt="" />
+      </div>
+      <div className="w-1/2">
+        <h1>{service.title}</h1>
+        <div className="flex items-center gap-1 mb-1">
+          <div
+            className={` ${
+              service.review ? "bg-green-500" : "bg-transparent"
+            } w-3 h-3 rounded-full border-2 border-green-500 `}
+          ></div>
+          <div
+            className={` ${
+              service.review ? "bg-green-500" : "bg-transparent"
+            } w-3 h-3  rounded-full border-2 border-green-500 `}
+          ></div>
+          <div
+            className={` ${
+              service.review ? "bg-green-500" : "bg-transparent"
+            } w-3 h-3  rounded-full border-2 border-green-500`}
+          ></div>
+          <div
+            className={` ${
+              service.review ? "bg-green-500" : "bg-transparent"
+            } w-3 h-3 rounded-full border-2 border-green-500`}
+          ></div>
+          <div className="text-sm text-gray-600 font-semibold ml-2"> 1 </div>
+              </div>
+              <p>{service.des}</p>
+              <div>
+                  <p>{service.price}</p>
+                  <button>Checkout</button>
+              </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailsServ;
