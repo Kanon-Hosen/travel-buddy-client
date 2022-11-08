5aa79a0ced7c1d0be77ea9c4e9dff3b5e0241f6d
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-11-08 21:17:17 +0600

    loader added

--- src/pages/DetailsServ.js
+++ src/pages/DetailsServ.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { BsHeart } from "react-icons/bs";
+import { FaRegCommentDots } from "react-icons/fa";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { auth } from "../config/Firebase";
@@ -8,18 +10,35 @@ import { auth } from "../config/Firebase";
 const DetailsServ = () => {
   const { id } = useParams();
   const [service, setService] = useState({});
-  const [user] = useAuthState(auth)
+  const [user, loading] = useAuthState(auth);
+  const username = user?.displayName?.replaceAll(' ', '')
+  console.log("🚀 ~ file: DetailsServ.js ~ line 15 ~ DetailsServ ~ username", username)
   const [review, setReview] = useState([]);
   useEffect(() => {
       fetch(`http://localhost:5000/review/${id}`)
           .then(res => res.json())
       .then(data=>setReview(data))
-   },[id])
+  }, [id])
+  
   useEffect(() => {
     fetch(`http://localhost:5000/services/${id}`)
       .then((res) => res.json())
       .then((data) => setService(data));
   }, [id]);
+
+
+  // loader added
+  if (loading) {
+    return <div className={`${loading ? 'visible' : 'hidden'} absolute top-16 left-0 z-50 flex items-center justify-center w-full h-screen bg-gray-900 bg-opacity-20`}>
+    <div role="status" className='z-50'>
+<svg className="inline z-50 mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+</svg>
+<span className="sr-only">Loading...</span>
+</div>
+    </div>
+  }
   return (
     <div>
       <div className="flex gap-5 my-5 w-full justify-between">
@@ -31,22 +50,22 @@ const DetailsServ = () => {
           <div className="flex items-center gap-1 my-4">
             <div
               className={` ${
-                review.reviewSerId  ? "bg-green-500" : "bg-transparent"
+                review[0]?._id  ? "bg-green-500" : "bg-transparent"
               } w-5 h-5 rounded-full border-2 border-green-500 `}
             ></div>
             <div
               className={` ${
-                review.reviewSerId  ? "bg-green-500" : "bg-transparent"
+                review[0]?._id  ? "bg-green-500" : "bg-transparent"
               } w-5 h-5  rounded-full border-2 border-green-500 `}
             ></div>
             <div
               className={` ${
-                review.reviewSerId  ? "bg-green-500" : "bg-transparent"
+                review[0]?._id  ? "bg-green-500" : "bg-transparent"
               } w-5 h-5  rounded-full border-2 border-green-500`}
             ></div>
             <div
               className={` ${
-              review.reviewSerId ? "bg-green-500" : "bg-transparent"
+                review[0]?._id ? "bg-green-500" : "bg-transparent"
               } w-5 h-5 rounded-full border-2 border-green-500`}
             ></div>
             <div className="text-sm text-gray-600 font-semibold ml-2 underline">{review.length} review </div>
@@ -67,8 +86,12 @@ const DetailsServ = () => {
       <div>
           {
             user ? <div>
-              <h1 className="my-10 text-3xl font-semibold">Contribute</h1>
+              {
+              <div>
+                <h1 className="my-10 text-3xl font-semibold">Contribute</h1>
               <Link to={`/${id}/writereview`} className="border-black border-2 px-5 py-4 rounded-full text-black font-semibold my-6 hover:bg-black hover:text-gray-50 transition-colors">Write a review</Link>
+                </div>
+                }
             </div> :
               <div className="">
                 <h1 className="text-3xl font-semibold my-4">You need to Sign in first</h1>
@@ -77,14 +100,46 @@ const DetailsServ = () => {
           }
         </div>
         <h1 className="text-2xl font-bold my-10 border-b-2">Reviews</h1>
-        <div>
+        <div >
           {
-            review.reviewSerId ? <div>
+            review[0]?._id ? <div className="flex flex-col gap-5">
               {
                 review.map(r => {
+                  const rating = parseInt(r.rating);
+                  console.log(rating)
                   return (
-                    <div>
-                      
+                    <div key={r._id} className="flex flex-col">
+                      <div className="flex gap-5">
+                        <div className="w-52 h-16 rounded-full">
+                        <img className="w-full ring-2 h-full rounded-full shadow" src={r?.image} alt="" />
+                        </div>
+                        <div className="flex flex-col"><p className="text-base font-semibold">{r?.username}</p>
+                        <div className="flex gap-1 mt-3">
+                        {
+                          [...Array(rating)].map((rating, i) => {
+                            return (
+                              <div key={i} className="w-5 h-5 rounded-full bg-green-500 flex  flex-col "></div>
+                            )
+                          })
+                            }
+                          </div>
+                          <div className="my-6">
+                      <p className="text-base font-semibold ">{r.reviewTitle}</p>
+                            <p className="mt-4 text-sm ">{r.reviewDes}</p>
+                            {
+                              user?.displayName === r.username ? <div className="mt-6 flex items-center gap-7">
+                              <p className="font-semibold text-base hover:underline transition-colors cursor-pointer">Edit</p>
+                              <p className="font-semibold text-base hover:underline transition-colors cursor-pointer">Delete</p>
+                              </div> : <div className="flex items-center gap-8 text-2xl">
+                                  <button className=" font-bold text-xl text-black  mt-5 rounded-full"><BsHeart></BsHeart></button>
+                                  <button className=" font-bold text-xl text-black  mt-5 rounded-full"><FaRegCommentDots className="focus:bg-red-500"></FaRegCommentDots></button>
+                            </div>
+                            }
+                      </div>
+                        </div>
+
+                      </div>
+
                     </div>
                   )
                 })
--- src/pages/WriteReview.js
+++ src/pages/WriteReview.js
@@ -1,80 +1,120 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useParams } from 'react-router';
-import { auth } from '../config/Firebase';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useParams } from "react-router";
+import { auth } from "../config/Firebase";
 
 const WriteReview = () => {
-    const { id } = useParams();
-    const [service, setService] = useState({});
-    const [user] = useAuthState(auth);
+  const { id } = useParams();
+  const [service, setService] = useState({});
+  const [user] = useAuthState(auth);
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/services/${id}`)
-            .then(res => res.json())
-            .then(data => {
-            setService(data)
-        })
-    }, [id])
-    
-    const handleReview = (e) => {
-        e.preventDefault();
+  useEffect(() => {
+    fetch(`http://localhost:5000/services/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setService(data);
+      });
+  }, [id]);
 
-        const reviewDes = e.target.des.value;
-        const reviewTitle = e.target.title.value;
-        const review = {
-            reviewDes,
-            reviewTitle,
-            reviewSerId: id
-        }
-        try {
-            fetch(`http://localhost:5000/review`, {
-                method: "POST",
-                headers: {
-                    "content-type":"application/json"
-                },
-                body: JSON.stringify(review)
-            })
-        } catch (error) {
-            console.log(error.message)
-        }
+  const handleReview = (e) => {
+    e.preventDefault();
+    const rating = e.target.rating.value;
+    const reviewDes = e.target.des.value;
+    const reviewTitle = e.target.title.value;
+    const username = e.target.name.value;
+    const image = e.target.image.value;
+    const review = {
+      rating,
+      username,
+      image,
+      reviewDes,
+      reviewTitle,
+      reviewSerId: id,
+    };
+    try {
+      fetch(`http://localhost:5000/review`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(review),
+      });
+    } catch (error) {
+      console.log(error.message);
     }
-    return (
-        <div className='my-10'>
-            <div className='flex items-center justify-between gap-5'>
-                <img className='w-56' src={service.image} alt="" />
-                <div>
-                    <h1 className='text-base font-semibold my-4'>{service.title}</h1>
-                    <p className='text-sm'>{service.des}</p>
-                </div>
-            </div>
-            <div className='my-10 border-b-2 '>
-                <h1 className='font-semibold text-xl my-4 '>Rate Your Experience</h1>
-                <div className='flex items-center gap-1 mb-5'>
-                    <div className='w-8 h-8 border-4 rounded-full border-green-500 cursor-pointer hover:bg-green-500 transition-colors'></div>
-                    <div className='w-8 h-8 border-4 rounded-full border-green-500 cursor-pointer hover:bg-green-500 transition-colors'></div>
-                    <div className='w-8 h-8 border-4 rounded-full border-green-500 cursor-pointer hover:bg-green-500 transition-colors'></div>
-                    <div className='w-8 h-8 border-4 rounded-full border-green-500 cursor-pointer hover:bg-green-500 transition-colors'></div>
-                    <div className='w-8 h-8 border-4 rounded-full border-green-500 cursor-pointer hover:bg-green-500 transition-colors'></div>
-                </div>
-            </div>
-            <form onSubmit={handleReview} className='flex flex-col gap-5'>
-       
-                <label htmlFor="name">User name</label>
-                <input type="text" className='border-2 p-4'  name='name' placeholder='Your name' defaultValue={user?.displayName}/>
-                <label htmlFor="image">Image url</label>
-                <input type="text" className='border-2 p-4' name='image' placeholder='Your image' defaultValue={user?.photoURL} />
-                <label htmlFor="des">Review title</label>
-                <textarea className='border-2 p-4 resize-none' name="title" id="" cols="2" rows="1" placeholder='Review title'></textarea>
-                <label htmlFor="des">Leave a review</label>
-                <textarea className='border-2 p-4 resize-none' name="des" id="" cols="4" rows="4" placeholder='What was your favorite part of your exprience?'></textarea>
-                <div className='w-72'>
-                <button className='px-10 py-3 w-full mt-3 bg-black text-gray-50'>Submit</button>
-                </div>
-            </form>
+  };
+  return (
+    <div className="my-10">
+      <div className="flex items-center justify-between gap-5">
+        <img className="w-56" src={service.image} alt="" />
+        <div>
+          <h1 className="text-base font-semibold my-4">{service.title}</h1>
+          <p className="text-sm">{service.des}</p>
         </div>
-    );
+      </div>
+      <div className="my-10 border-b-2 ">
+        <h1 className="font-semibold text-xl my-4 ">Rate Your Experience</h1>
+      </div>
+      <form onSubmit={handleReview} className="flex flex-col gap-5">
+        <label htmlFor="rating">Choose Rating:</label>
+
+        <select name="rating" className="p-4 border-2" required>
+          <option value="1">1 star rating</option>
+          <option value="2">2 star rating</option>
+          <option value="3">3 star rating</option>
+          <option value="4">4 star rating</option>
+          <option value="5" selected>
+            5 star rating
+          </option>
+        </select>
+        <label htmlFor="name">User name</label>
+        <input
+          required
+          type="text"
+          className="border-2 p-4"
+          name="name"
+          placeholder="Your name"
+          defaultValue={user?.displayName}
+        />
+        <label htmlFor="image">Image url</label>
+        <input
+          required
+          type="text"
+          className="border-2 p-4"
+          name="image"
+          placeholder="Your image"
+          defaultValue={user?.photoURL}
+        />
+        <label htmlFor="des">Review title</label>
+        <textarea
+          required
+          className="border-2 p-4 resize-none"
+          name="title"
+          id=""
+          cols="2"
+          rows="1"
+          placeholder="Review title"
+        ></textarea>
+        <label htmlFor="des">Leave a review</label>
+        <textarea
+          required
+          className="border-2 p-4 resize-none"
+          name="des"
+          id=""
+          cols="4"
+          rows="4"
+          placeholder="What was your favorite part of your exprience?"
+        ></textarea>
+        <div className="w-72">
+          <button className="px-10 py-3 w-full mt-3 bg-black text-gray-50">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default WriteReview;
